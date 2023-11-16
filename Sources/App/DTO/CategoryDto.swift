1c9c2dce36b865ed5c34c00b18fc9c9046bdf365
JasperLefever <jasper.lefever@telenet.be>
2023-11-16 19:13:30 +0100

    removed not usable prop

--- Sources/App/DTO/CategoryDto.swift
+++ Sources/App/DTO/CategoryDto.swift
@@ -4,7 +4,6 @@ struct CategoryResult: Content {
   var id: UUID?
   var name: String
   var icon: String
-  var amountOfQuestions: Int?
 
   init(category: Category) {
     self.id = category.id
@@ -12,10 +11,4 @@ struct CategoryResult: Content {
     self.icon = category.icon
   }
 
-  init(category: Category, amountOfQuestions: Int) {
-    self.id = category.id
-    self.name = category.name
-    self.icon = category.icon
-    self.amountOfQuestions = amountOfQuestions
-  }
 }
