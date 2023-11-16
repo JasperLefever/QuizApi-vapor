e6fef9a8e0f6520d72bed4c2eda72143b952abea
JasperLefever <jasper.lefever@telenet.be>
2023-11-16 14:03:17 +0100

    Forgot init

---
+++ Sources/App/Controllers/CategoryController.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  
+//
+//  Created by Jasper Lefever on 16/11/2023.
+//
+
+import Foundation
---
+++ Sources/App/Models/Category.swift
@@ -0,0 +1,9 @@
+//
+//  File.swift
+//  
+//
+//  Created by Jasper Lefever on 16/11/2023.
+//
+
+import Vapor
+import Fluent
