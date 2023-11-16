eae31e8a77ea880499d0b2cecb4783f79c959ba6
JasperLefever <jasper.lefever@telenet.be>
2023-11-16 18:35:02 +0100

    removed unused pagination dto

--- Sources/App/DTO/PaginationDto.swift
+++ Sources/App/DTO/PaginationDto.swift
@@ -1,18 +1,6 @@
 import Vapor
 
-struct PageResult<T: Content>: Content {
-  var metadata: PageMetadata
-  var items: [T]
-}
-
 struct PageRequest: Content {
   var page: Int?
   var perPage: Int?
-}
-
-struct PageMetadata: Content {
-  var page: Int
-  var perPage: Int
-  var totalItems: Int
-  var totalPages: Int
 }
